a74eb06616ce8906028d90a278cc4b6f2affda03
Leul Yiheyis <leulykpro@gmail.com>
2025-05-27 20:46:57 +0300

    implement button component and test

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,21 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button"
 
 const Landing: React.FC = () => {
     return (
         <div>
             <h1 className=" text-xl font-extralight">Landing Page</h1>
             <Card />
+            <div>
+                <Button title="Submit" styles="m-10 w-2xs h-[30px] rounded-md bg-orange-300 text-black" />
+            </div>
+            <div>
+                <Button title="Save" styles="m-10 w-md h-[50px] rounded-sm bg-blue-200 text-black" />
+            </div>
+            <div>
+                <Button title="Apply" styles="m-10 w-2xl h-[35px] rounded-full bg-yellow-100 text-black" />
+                <Button title="Apply" styles="m-10 w-2xl h-[35px] rounded-lg bg-yellow-100 text-black" />
+            </div>
         </div>
     )
 }
